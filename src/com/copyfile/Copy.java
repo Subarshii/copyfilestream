d7eb653e56b59e1edb20759b33b245aa9ce6a66d
chatchai <s14_110@hotmail.com>
2019-07-09 17:06:31 +0700

    .

--- src/com/copyfile/Copy.java
+++ src/com/copyfile/Copy.java
@@ -6,22 +6,21 @@ public class Copy {
     public static void main(String[] args) {
 
 
-        try (InputStream r = new FileInputStream("src/com/copyfile/image.jpg")) {
+        try (InputStream r = new FileInputStream("src/com/copyfile/image.jpg");
+             OutputStream w = new FileOutputStream("src/com/copyfile/image_2.jpg")){
 
-            try (OutputStream w = new FileOutputStream("src/com/copyfile/image_2.jpg")) {
 
-                while (true) {
-                    int s;
-                    if ((s = r.read()) == -1) {
-                        break;
-                    }
-                    System.out.println((char) s + " = " + s);
-                    w.write(s);
+            while (true) {
+                int s;
+                if ((s = r.read()) == -1) {
+                    break;
                 }
+                System.out.println((char) s + " = " + s);
+                w.write(s);
             }
 
 
-        } catch (IOException ex) {
+        } catch(IOException ex){
             System.out.println("IOException" + ex);
         }
 
