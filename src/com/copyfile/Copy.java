e8364060c57a8131cd12c4d9583cbb82c47d44fa
chatchai <s14_110@hotmail.com>
2019-07-09 16:39:20 +0700

    .

--- src/com/copyfile/Copy.java
+++ src/com/copyfile/Copy.java
@@ -3,29 +3,26 @@ package com.copyfile;
 import java.io.*;
 
 public class Copy {
-    public static void main(String[] args) throws IOException {
-        int s;
-        InputStream r = null;
-        OutputStream w = null;
-        try {
-            r = new FileInputStream("src/com/copyfile/text.txt");
-            w = new FileOutputStream("src/com/copyfile/text_2.txt");
+    public static void main(String[] args) {
 
-            while ((s = r.read()) != -1) {
-                System.out.println((char) s + " = " + s);
-                w.write(s);
+
+        try (InputStream r = new FileInputStream("src/com/copyfile/image.jpg")) {
+
+            try (OutputStream w = new FileOutputStream("src/com/copyfile/image_2.jpg")) {
+
+                while (true) {
+                    int s;
+                    if ((s = r.read()) == -1) {
+                        break;
+                    }
+                    System.out.println((char) s + " = " + s);
+                    w.write(s);
+                }
             }
 
 
         } catch (IOException ex) {
             System.out.println("IOException" + ex);
-        } finally {
-            if (r != null) {
-                r.close();
-            }
-            if (w != null) {
-                r.close();
-            }
         }
 
 
